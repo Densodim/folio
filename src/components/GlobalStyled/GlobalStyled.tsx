f1a8ebbe9b411780a3bfb2bb5b167ab5c14c4c78
Densodim <25denso@gmail.com>
2024-04-18 21:30:26 +0300

    jjj

---
+++ src/components/GlobalStyled/GlobalStyled.tsx
@@ -0,0 +1,15 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+export const GlobalStyled = createGlobalStyle`
+    *,
+    *::before,
+    *::after {
+        padding: 0px;
+        margin: 0px;
+    }
+    body{
+        font-family: 'Poppins', sans-serif;
+        background-color: black;
+        color: white;
+    }
+`
---
+++ src/components/Header/header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <>
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+
+    </>
+  );
+};
